55e610fbfa1b81368c60a08373c22dcf140dcba5
Satyam Pund <pundsatyam@gmail.com>
2023-12-27 00:29:34 +0530

    feat: advance context API

---
+++ src/PostContext.js
@@ -0,0 +1,48 @@
+import { createContext, useState } from 'react';
+import { faker } from '@faker-js/faker';
+
+//1. CREATE A CONTEXT
+const PostContext = createContext();
+
+function createRandomPost() {
+  return {
+    title: `${faker.hacker.adjective()} ${faker.hacker.noun()}`,
+    body: faker.hacker.phrase(),
+  };
+}
+
+function PostProvider({ children }) {
+  const [posts, setPosts] = useState(() => Array.from({ length: 30 }, () => createRandomPost()));
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const searchedPosts =
+    searchQuery.length > 0
+      ? posts.filter((post) =>
+          `${post.title} ${post.body}`.toLowerCase().includes(searchQuery.toLowerCase())
+        )
+      : posts;
+
+  function handleAddPost(post) {
+    setPosts((posts) => [post, ...posts]);
+  }
+
+  function handleClearPosts() {
+    setPosts([]);
+  }
+
+  return (
+    //2. PROVIDE VALUE TO CHILD COMPONENTS
+    <PostContext.Provider
+      value={{
+        posts: searchedPosts,
+        onClearPosts: handleClearPosts,
+        onAddPost: handleAddPost,
+        searchQuery,
+        setSearchQuery,
+      }}>
+      {children}
+    </PostContext.Provider>
+  );
+}
+
+export { PostProvider, PostContext };
